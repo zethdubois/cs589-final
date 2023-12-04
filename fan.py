74a34ba4632fb73b6d84c771008762c131299d93
Zeth <fixrun@pm.me>
2023-12-04 15:38:20 +0100

    5 data sets

--- fan.py
+++ fan.py
@@ -4,6 +4,13 @@ from tkinter import Tk, StringVar, OptionMenu, Scale, HORIZONTAL, Label
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
+# Define your element_files dictionary
+element_files = {
+    'Lithium': 'LI-query_filtered.csv',
+    'Aluminum': 'AL-query_filtered.csv',
+    # ... other elements
+}
+
 # Function to plot the selected element
 def plot_element(element, start_year, num_results):
     plt.clf()  # Clear the current figure
@@ -30,26 +37,52 @@ def dropdown_changed(*args):
     update_sliders(selected_element.get())
     plot_element(selected_element.get(), start_year_slider.get(), num_results_slider.get())
 
+# Function to update the plot based on sliders
+def update_plot(*args):
+    plot_element(selected_element.get(), start_year_slider.get(), num_results_slider.get())
 
+def update_sliders(element):
+    if element != "Select Mineral":
+        # Load data
+        data = pd.read_csv(element_files[element], encoding='ISO-8859-1')
+
+
+        # Update start_year_slider range and value
+        new_min_year = data['discoveryYear'].min()
+        new_max_year = data['discoveryYear'].max()
+        start_year_slider.config(from_=new_min_year, to=new_max_year,label=f"Start Year ({new_min_year} - {new_max_year})")
+        start_year_slider.set(new_min_year)
+
+        # Update num_results_slider
+        new_max_results = len(data)
+        num_results_slider.config(to=new_max_results, label=f"Number of Results (max {new_max_results})")
+        num_results_slider.set(new_max_results // 2)
+
+        # Enable sliders
+        start_year_slider.config(state='normal')
+        num_results_slider.config(state='normal')
+
+
+
+        plot_element(element, start_year_slider.get(), num_results_slider.get())    
+    else:
+        # Disable sliders
+        start_year_slider.config(state='disabled')
+        num_results_slider.config(state='disabled')
+
+    # Redraw the plot with updated values
+
+
+#-----------------------------------------MAIN
 # Initialize Tkinter window
 root = Tk()
 root.title("RDF Mineral Chart")
 
-# Define your element_files dictionary
-element_files = {
-    'Lithium': 'LI-query_filtered.csv',
-    'Aluminum': 'AL-query_filtered.csv',
-    # ... other elements
-}
-
 # Dropdown menu for element selection
 selected_element = StringVar(root)
-selected_element.set(list(element_files.keys())[0])  # Set default value
-
-# Load initial data to set slider range
-initial_data = pd.read_csv(element_files[selected_element.get()])
-initial_min_year = initial_data['discoveryYear'].min()
-initial_max_year = initial_data['discoveryYear'].max()
+selected_element.set("Select Mineral")  # Initial text
+dropdown = OptionMenu(root, selected_element, *element_files.keys(), command=dropdown_changed)
+dropdown.grid(row=0, column=0, sticky="ew")
 
 # Create a matplotlib figure
 fig = plt.figure(figsize=(10, 6))
@@ -57,56 +90,18 @@ canvas = FigureCanvasTkAgg(fig, master=root)
 widget = canvas.get_tk_widget()
 widget.grid(row=1, column=0, columnspan=3, sticky="nsew")
 
-# Load initial data to set slider range
-initial_data = pd.read_csv(element_files[selected_element.get()])
-initial_min_year = initial_data['discoveryYear'].min()
-initial_max_year = initial_data['discoveryYear'].max()
+# Starting Year Slider (Initially Disabled)
+start_year_slider = Scale(root, from_=1800, to=2023, orient=HORIZONTAL, label="Start Year", state='disabled')
+start_year_slider.grid(row=2, column=0, sticky="ew")
 
-# Starting Year Slider
-start_year_slider = Scale(root, from_=initial_min_year, to=initial_max_year, orient=HORIZONTAL, label="Start Year")
-start_year_slider.grid(row=2, column=0, columnspan=1, sticky="ew")
+# Number of Results Slider (Initially Disabled)
+initial_num_results = min(1, 2)  # Adjust as needed
+num_results_slider = Scale(root, from_=1, to=initial_num_results, orient=HORIZONTAL, label="Number of Results", state='disabled')
+num_results_slider.grid(row=2, column=1, sticky="ew")
 
-# Number of Results Slider
-initial_num_results = min(100, len(initial_data))  # Adjust this as needed
-num_results_slider = Scale(root, from_=1, to=initial_num_results, orient=HORIZONTAL, label="Number of Results")
-num_results_slider.grid(row=2, column=1, columnspan=1, sticky="ew")
-
-# Configure grid
-root.grid_rowconfigure(0, weight=1)
-root.grid_columnconfigure(1, weight=1)
-
-# Dropdown menu for element selection
-selected_element = StringVar(root)
-selected_element.set(list(element_files.keys())[0])  # Set default value
-
-# When element is changed via dropdown
-dropdown = OptionMenu(root, selected_element, *element_files.keys(), command=dropdown_changed)
-dropdown.grid(row=0, column=0, sticky="ew")
-
-
-# Function to update the plot based on sliders
-def update_plot(*args):
-    plot_element(selected_element.get(), start_year_slider.get(), num_results_slider.get())
-
-def update_sliders(element):
-    # Load data
-    data = pd.read_csv(element_files[element], encoding='ISO-8859-1')
-
-    # Update start_year_slider range and value
-    new_min_year = data['discoveryYear'].min()
-    new_max_year = data['discoveryYear'].max()
-    start_year_slider.config(from_=new_min_year, to=new_max_year,label=f"Start Year ({new_min_year} - {new_max_year})")
-    start_year_slider.set(new_min_year)
-
-    # Update num_results_slider
-    new_max_results = len(data)
-    num_results_slider.config(to=new_max_results, label=f"Number of Results (max {new_max_results})")
-    num_results_slider.set(new_max_results // 2)
-
-    # Redraw the plot with updated values
-    plot_element(element, start_year_slider.get(), num_results_slider.get())
-
-
+# Manually invoke 'update_sliders' for the first element in the list
+initial_element = list(element_files.keys())[0]
+update_sliders(initial_element)
 
 # Bind sliders and dropdown to update_plot function
 start_year_slider.bind("<ButtonRelease-1>", update_plot)
@@ -117,6 +112,5 @@ selected_element.trace("w", update_plot)
 root.grid_rowconfigure(1, weight=1)
 root.grid_columnconfigure(0, weight=1)
 root.grid_columnconfigure(1, weight=1)
-
 
 root.mainloop()
